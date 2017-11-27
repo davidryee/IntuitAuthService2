8bb403f011090463597a335c7d856d1889f9f194
David Yee <davidryee@gmail.com>
2017-11-26 18:56:56 -0800

    Added feature for looking up by username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,15 @@ class UsersController < ApplicationController
     redirect_to @user
   end
 
+
   def show
     @user = User.find(params[:id])
   end
 
+  def show_by_username
+    @user = User.find_by(username: params[:username])
+    redirect_to @user
+  end
   private
     def user_params
       params.require(:user).permit(:username, :password)
